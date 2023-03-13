eff142ec4a0a23e8156c9645d2f276f5046f18a9
Michelle Santiago <mdcsantiago0@gmail.com>
2023-03-13 19:15:43 +0800

    initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,53 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    # @article.name = params[:name]
+    # @article.body = params[:body]
+
+    if @article.save
+      redirect_to articles_path
+    else 
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params["id"])
+  end
+
+  def update
+    @article = Article.find(params["id"])
+
+    if @article.update(article_params)
+      redirect_to articles_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def delete
+    @article = Article.find(params["id"])
+    @article.destroy
+    redirect_to articles_path
+    # else
+    #   render :edit, status: :unprocessable_entity
+    # end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:name,:body)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  
+  # root "articles#index"
+  get '/articles' => 'articles#index'
+  get '/articles/new' => 'articles#new', as: 'new_article'
+  get '/articles/:id/edit' => 'articles#edit', as: 'edit_article'
+  post 'articles' => 'articles#create', as: 'create_article'
+  patch '/articles/:id' => 'articles#update', as: 'update_article'
+  delete '/articles/:id' => 'articles#delete', as: 'delete_article'
+
+end
