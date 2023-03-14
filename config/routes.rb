90d421cf5397f38b95f1c1da64496797c5542bcc
Michelle Santiago <mdcsantiago0@gmail.com>
2023-03-14 12:15:55 +0800

    refactor: url and added show article

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,13 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   
-  # root "articles#index"
+  root "articles#index"
   get '/articles' => 'articles#index', as: 'articles'
   get '/articles/new' => 'articles#new', as: 'new_article'
   get '/articles/:id/edit' => 'articles#edit', as: 'edit_article'
   post 'articles' => 'articles#create', as: 'create_article'
   patch '/articles/:id' => 'articles#update', as: 'update_article'
   delete '/articles/:id' => 'articles#delete', as: 'delete_article'
+  get '/articles/:id' => 'articles#show', as: 'show_article'
 
 end
